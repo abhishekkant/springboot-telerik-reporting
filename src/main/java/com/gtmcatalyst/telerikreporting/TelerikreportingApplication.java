9b96eb0a40e62a1b7a1998339bbf7ba102e6ab49
Abhishek Kant <abhishek@abhishekkant.net>
2021-01-03 16:53:45 +0530

    Initial Commit

---
+++ src/main/java/com/gtmcatalyst/telerikreporting/TelerikreportingApplication.java
@@ -0,0 +1,13 @@
+package com.gtmcatalyst.telerikreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelerikreportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelerikreportingApplication.class, args);
+	}
+
+}
